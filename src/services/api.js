98c13570a29bb75188387385b77b97b8a89a2064
Wellington Leite <wellingtonleitee.s@gmail.com>
2020-02-03 23:07:41 -0300

    troca url da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '../store';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://vuttrapi.herokuapp.com:3000',
 });
 
 api.interceptors.request.use(async config => {
